353ffb3229078fcbe8ffe0e9a3cdcd0542487f2f
Ilian Hadzhidimitrov <ilian_hadzhidimitrov99@abv.bg>
2024-09-24 15:27:37 +0300

    Added difficulty levels and limited attempts

--- _guess_the_number_.py
+++ _guess_the_number_.py
@@ -1,8 +1,26 @@
 import random
-
 computer_choice = random.randint(1, 100)
-while True:
-    player_input = input('Guess the number (1, 100): ')
+difficulty_level = input('Enter a difficulty level between 1 and 3: ')
+count_attempts = 0
+number_range = 0
+
+if difficulty_level == '1':
+   count_attempts = 20
+   number_range = range(1, 101)
+   print(f'You will have {count_attempts} attempts to guess the number')
+elif difficulty_level == '2':
+    count_attempts = 10
+    number_range = range(1, 201)
+    print(f'You will have {count_attempts} attempts to guess the number')
+elif difficulty_level == '3':
+    count_attempts = 5
+    number_range = range(1, 301)
+    print(f'You will have {count_attempts} attempts to guess the number')
+else:
+    print('Invalid input!Try again...')
+
+for _ in range(count_attempts):
+    player_input = input(f'Guess the number {number_range}: ')
 
     if not player_input.isdigit():
         print('Invalid input! Try again...')
@@ -16,5 +34,5 @@ while True:
         print('Too High!')
     elif player_number < computer_choice:
         print('Too Low!')
-
-
+else:
+    print('Sorry you ran out of attempts')
